ba1693af31059e72acd8906ea9cb24bcd8a0a9c8
Andres Bermúdez <a.arango09@gmail.com>
2020-09-14 17:29:01 -0500

    learn

--- ES8.js
+++ ES8.js
@@ -30,3 +30,35 @@ const string = 'hello';
 console.log(string.padStart(7, 'hi'))
 console.log(string.padEnd(12, '--------hi'))
 console.log('food'.padEnd(12, '--------hi'))
+
+
+
+///// async  ' espera hasta que se cumpla la promesa
+
+const helloWord = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ? setTimeout(() => resolve('hello world'), 3000)
+        : reject(new Error(' Test Error'))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await helloWord();
+    console.log(hello);
+}
+
+helloAsync()
+
+
+///// manejo de errores
+
+const anothofFunction = async () => {
+    try {
+        const hello = await helloWord();
+        console.log(hello)
+    }  catch (error) {
+        console.log(error)
+    }
+
+}
